9d491d1653b4ea0275f9e12b2dec5ac959923f8f
Naman Tyagi <namantyagi98665@gmail.com>
2026-08-11 13:12:50 +0530

    deployment setup

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175']
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
+  process.env.FRONTEND_URL
+].filter(Boolean);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins , credentials : true}));
